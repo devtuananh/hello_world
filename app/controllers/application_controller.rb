dfae35c10bc6052449ff2bdd2533c6623f861a14
devtuananh <tran.tuan.anhb@framgia.com>
2019-05-18 10:57:47 +0700

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
     def index
     end
+
     def new
     end
+
     def create
+    end
+    def edit
     end
 
     def update
     end
-    def destroy
+ 
+   def destroy
     end
 end
